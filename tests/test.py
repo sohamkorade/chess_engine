2bbe9e93fcc487ad51ca84eee0a34c0a6c68ef1d
sohamkorade <sohamkorade.jnvkk@gmail.com>
2024-07-09 23:20:43 +0530

    improve engine tester

--- tests/test.py
+++ tests/test.py
@@ -2,7 +2,6 @@ import chess
 import chess.engine
 import chess.pgn
 import argparse
-import random
 import time
 import tqdm
 import os
@@ -23,26 +22,21 @@ engine2_name=os.path.basename(args.engine2)
 
 pgn=open(f"games/{engine1_name} vs {engine2_name} at {time.strftime('%Y-%m-%d %H:%M:%S')}.pgn", "a+")
 
-def match(round):
-	engine1=chess.engine.SimpleEngine.popen_uci(args.engine1)
-	engine2=chess.engine.SimpleEngine.popen_uci(args.engine2)
-
+def match(round,white,black):
 	board = chess.Board()
 	wtime=0.01
 	btime=0.01
-	turn=random.choice([engine1, engine2])
+	turn = white
 
-	# print("W: ", args.engine1)
-	# print("B: ", args.engine2)
-
-	for i in tqdm.trange(100):
-		result = turn.play(board, chess.engine.Limit(time=wtime))
+	for i in range(100):
+		time_limit=wtime if turn == white else btime
+		result = turn.play(board, chess.engine.Limit(depth=2))
 		if result.move == None:
 			break
 		board.push(result.move)
 		if board.is_game_over():
 			break
-		turn = engine1 if turn == engine2 else engine2
+		turn = black if turn == white else white
 		
 	# else:
 		# print("Game too long: aborted.")
@@ -50,17 +44,15 @@ def match(round):
 	# print(board.result())
 	# print(board)
 
-	engine1.quit()
-	engine2.quit()
 
 	# save game
 	game=chess.pgn.Game.from_board(board)
 	game.headers["Event"]="Engine match"
 	game.headers["Site"]="Local"
 	game.headers["Round"]=round
-	game.headers["Date"]=time.strftime('%Y/%m/%d %H:%M:%S')
-	game.headers["White"]=args.engine1
-	game.headers["Black"]=args.engine2
+	# game.headers["Date"]=time.strftime('%Y/%m/%d %H:%M:%S')
+	game.headers["White"]=white.id["name"]
+	game.headers["Black"]=black.id["name"]
 	game.headers["Result"]=board.result()
 
 	print(game, file=pgn, end="\n\n")
@@ -68,31 +60,39 @@ def match(round):
 	return board.result()
 
 
-def runall():
-	white = []
-	black = []
-	white_sum = 0
-	black_sum = 0
-	for i in range(10):
-		result= match(i)
-		if result == "1-0":
-			white.append('1')
-			black.append('0')
-			white_sum += 1
-		elif result == "0-1":
-			white.append('0')
-			black.append('1')
-			black_sum += 1
-		elif result == "1/2-1/2":
-			white.append('½')
-			black.append('½')
-			white_sum += 0.5
-			black_sum += 0.5
-		else:
-			white.append('*')
-			black.append('*')
-		
-	print("White:"," ".join(white), "=", white_sum)
-	print("Black:"," ".join(black), "=", black_sum)
+def runall(e1_path=args.engine1, e2_path=args.engine2):
+	engine1=chess.engine.SimpleEngine.popen_uci(e1_path)
+	engine2=chess.engine.SimpleEngine.popen_uci(e2_path)
 
-runall()
+	print(engine1.id["name"], "vs", engine2.id["name"])
+
+	scores={
+		engine1: [],
+		engine2: [],
+	}
+
+	white, black = engine1, engine2
+	for i in tqdm.trange(10):
+		result= match(i+1, white, black)
+		if result == "1-0":
+			scores[white].append((1,'1'))
+			scores[black].append((0,'0'))
+		elif result == "0-1":
+			scores[white].append((0,'0'))
+			scores[black].append((1,'1'))
+		elif result == "1/2-1/2":
+			scores[white].append((0.5,'½'))
+			scores[black].append((0.5,'½'))
+		else:
+			scores[white].append((0,'*'))
+			scores[black].append((0,'*'))
+	
+	for engine in [engine1, engine2]:
+		print(f"{engine.id['name']}:", " ".join(text for score,text in scores[engine]), "=", sum(score for score,text in scores[engine]))
+
+	engine1.quit()
+	engine2.quit()
+
+
+runall(args.engine1, args.engine2)
+runall(args.engine2, args.engine1)
