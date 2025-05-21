sohamkorade/chess_engine

commit d0a1c8c42c334f24e4c63f3abb6e7d39353ce328

refactor: simplify promotion move generation in generate_legal_moves2

src/movegen.cpp
@@ -729,22 +729,7 @@ vector<Move> generate_legal_moves2(Board& board) {
               only_capture<rel_NE>(pos, movelist, sq);
             }
           } else {  // promotion moves
-            // TODO: keep only below line
-            // generate_promotion_moves_safe<turn>(pos, movelist, sq);
-
-            // TODO: remove this
-            vector<Piece> promotions = {rel_Q, rel_R, rel_B, rel_N};
-            if (attacker_dir == N || attacker_dir == S) {
-              generate_promotion_moves_safe<turn>(pos, movelist, sq);
-            } else if (attacker_dir == rel_NW) {
-              if (isnt_A(sq) && hostile(pos[sq], pos[sq + rel_NW]))
-                for (auto& piece : promotions)
-                  movelist.emplace_back(sq, sq + rel_NW, piece);
-            } else if (attacker_dir == rel_NE) {
-              if (isnt_H(sq) && hostile(pos[sq], pos[sq + rel_NE]))
-                for (auto& piece : promotions)
-                  movelist.emplace_back(sq, sq + rel_NE, piece);
-            }
+            generate_promotion_moves_safe<turn>(pos, movelist, sq);
           }
         }
 // slide_capture_threat<attacker_dir, rel_B, rel_Q, rel_K>(pos, movelist, sq);

commit 3bbd4e7bb3fd5680d9051c15f81aacdf8ce50831

bugfix: fixes bug introduced by commit d0a1c8c

src/movegen.cpp
@@ -729,7 +729,21 @@ vector<Move> generate_legal_moves2(Board& board) {
               only_capture<rel_NE>(pos, movelist, sq);
             }
           } else {  // promotion moves
-            generate_promotion_moves_safe<turn>(pos, movelist, sq);
+#define promote(dir)                               \
+  for (auto& piece : {rel_Q, rel_R, rel_B, rel_N}) \
+    movelist.emplace_back(sq, sq + dir, piece);
+
+            if (attacker_dir == N || attacker_dir == S) {
+              if (pos[sq + rel_North] == Empty) promote(rel_North);
+            } else if (attacker_dir == rel_NW) {
+              if (isnt_A(sq) && hostile(pos[sq], pos[sq + rel_NW]))
+                promote(rel_NW);
+            } else if (attacker_dir == rel_NE) {
+              if (isnt_H(sq) && hostile(pos[sq], pos[sq + rel_NE]))
+                promote(rel_NE);
+            }
+
+#undef promote
           }
         }
 // slide_capture_threat<attacker_dir, rel_B, rel_Q, rel_K>(pos, movelist, sq);